63a4fe8d7d5a1c52dd1b87b0e20cc002c1987d44
zjxps2007 <zjxps2007@gmail.com>
2019-09-23 16:16:28 +0900

    HomeWork_2

--- Hw2/src/Hw2.java
+++ Hw2/src/Hw2.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
 
+//클래스 Car 생성
 class Car {
+    //필드변수 선언
     public String color = "Blue";
     public double speed = 10.4;
     public int gear = 1;
 
+    //기어변속 메소드
     void ChangeGear(int gear_c) {
         gear = gear_c;
     }
+    //속도 증가 메소드
     void SpeedUp() {
         speed += 10;
     }
+    //속도 저하 메소드
     void SpeedDown() {
+
         speed -=10;
     }
+    //정보 출력
     void CarPrint() {
         System.out.println("차 색상: "+color);
         System.out.println("기어: "+gear);
@@ -23,6 +30,7 @@ class Car {
 
 public class Hw2 {
     public static void main(String[] args) {
+        //변수선언
         int num =0;
         int gear;
 
@@ -31,9 +39,13 @@ public class Hw2 {
 
         car.CarPrint();
         System.out.println("----------------------");
+
+        //-1이 나올때까지 반복
         while (num != -1) {
+            //동작을 입력받음
             System.out.print("수행할 동작 입력(기어변속: 1, 가속: 2, 감속: 3, 종료: -1) :");
             num = sc.nextInt();
+            //입력받은 동작을 판별 & 살행
             switch (num) {
                 case 1:
                     System.out.print("몇 단으로 변속할지 입력:");
